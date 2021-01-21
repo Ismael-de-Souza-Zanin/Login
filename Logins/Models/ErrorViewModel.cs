6bef4391a360f543131b8b6158864f56dc41b5d0
Ismael de Souza Zanin <mt100388@interfile.com.br>
2021-01-21 14:23:36 -0300

    Criacao do Projeto MVC de Login

---
+++ Logins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Logins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
